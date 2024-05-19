43ebd746cb52b3ea203252852ce8011e931cdc86
Jcam2346 <jamescampbell@hotmail.com>
2024-05-19 15:04:27 +1000

    attempted mean_var_std submission

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,18 @@
 import numpy as np
 
-def calculate(list):
+def calculate(data: list) -> dict:
+    if len(data) != 9:
+        raise ValueError("List must contain nine numbers.")
+    
+    dataarray = np.array(data).reshape((3,3))
 
-
-
-
+    calculations = {
+        'mean': [list(dataarray.mean(axis=0)), list(dataarray.mean(axis=1)), dataarray.mean()],
+        'variance': [list(dataarray.var(axis=0)), list(dataarray.var(axis=1)), dataarray.var()],
+        'standard deviation': [list(dataarray.std(axis=0)), list(dataarray.std(axis=1)), dataarray.std()],
+        'max': [list(dataarray.max(axis=0)), list(dataarray.max(axis=1)), dataarray.max()],
+        'min': [list(dataarray.min(axis=0)), list(dataarray.min(axis=1)), dataarray.min()],
+        'sum': [list(dataarray.sum(axis=0)), list(dataarray.sum(axis=1)), dataarray.sum()]
+    }
+        
     return calculations
